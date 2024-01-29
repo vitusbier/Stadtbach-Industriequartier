0485bcc0628bf1dafbb496cef7342a999e04eb12
julio204 <154965087+julio204@users.noreply.github.com>
2024-01-29 16:02:33 +0100

    Add files via upload

--- Marktalgorithmus.py
+++ Marktalgorithmus.py
@@ -1,6 +1,141 @@
+# -*- coding: utf-8 -*-
+# Importieren der benötigten Bibliotheken und Klassen
 import mesa
 import numpy as np
+from Optimierung_Marketalgorithmus import Optimierung_Marketalgorithmus
+from SWA import SWA
+from Unternehmen import Unternehmen
+from Storage import Storage
+from DataBase import DataBase
 
-class MarketClearing (mesa.Model):
-    def __init__(self, simulation_data):
+# Die Hauptklasse für den Marktalgorithmus, die von mesa.Model erbt        
+class Marktalgorithmus(mesa.Model):
+    def __init__(self, simulation_data, level_t_0):
+        super().__init__()
+        # Initialisierung der Datenbank mit externen Simulationseinstellungen
+        self.database = DataBase(self, simulation_data) #Die Datei muss noch erstellt werden
         
+        # Anlegen Zeitindex
+        ti = self.model.time_index
+        rti = list(range(len(ti)))
+        
+        # Anlegen der Variablen, die später benötigt werden
+        self.results_MAN = []
+        self.results_MTA = []
+        self.results_UPM = []
+        self.results_SWA = []
+        self.results_storage = []
+        self.results_P2P_trading = []
+        self.tele_damand = []
+        self.tele_supply = []
+        self.Res_pre = []
+        self.level_t = []
+        
+        self.MAN_agent = None
+        self.MTA_agent = None
+        self.UPM_agent = None
+        self.storage_agent = None
+        self.SWA_agent = None
+        
+        self.epsilon_average = []
+        self.epsilon_MAN = []
+        self.epsilon_MTA = []
+        self.epsilon_UPM = []
+        self.epsilon_storage = []
+        self.epsilon_SWA = []
+        
+        # Initialisierung der Agenten und Aktivierungsplan für jeden Zeitschritt
+        for i in rti:
+            self.agents = []
+            self.schedule = mesa.time.RandomActivation(self)
+            
+            # Initialisierung der Unternehmen (Agents) für MAN, MTA und UPM für den aktuellen Zeitschritt 
+            self.MAN_agent = Unternehmen("MAN_agent", self, i, self.results_MAN, self.results_P2P_trading)
+            self.agents.append(self.MAN_agent)
+            self.schedule.add(self.MAN_agent)
+            self.MTA_agent = Unternehmen("MTA_agent", self, i, self.results_MTA, self.results_P2P_trading)
+            self.agents.append(self.MTA_agent)
+            self.schedule.add(self.MTA_agent)
+            self.UPM_agent = Unternehmen("UPM_agent", self, i, self.results_UPM, self.results_P2P_trading)
+            self.agents.append(self.UPM_agent)
+            self.schedule.add(self.UPM_agent)
+            
+            # (Neu-)Berechnung der Parameter für den aktuellen Zeitschritt
+            if i==0:
+                self.level_t.append(level_t_0)
+                self.Res_pre.append(0)
+            else:
+                self.level_t.append(self.results_storage[i-1][2]+self.results_P2P_trading[i-1][10]-self.results_P2P_trading[i-1][11])
+                self.tele_damand.append(self.results_MAN[i-1][9] + self.results_MAN[i-1][9] + self.results_MAN[i-1][9])
+                self.tele_supply.append(self.results_MAN[i-1][10] + self.results_MAN[i-1][10] + self.results_MAN[i-1][10])
+                self.Res_pre.append(self.tele_damand-self.tele_supply)
+                
+            # Initialisierung der Storage- und SWA-Agenten für den aktuellen Zeitschritt    
+            self.storage_agent = Storage("storage_agent", self, self.level_t, self.tele_damand, self.tele_supply, self.results_storage, i)
+            self.agents.append(self.storage_agent)
+            self.schedule.add(self.storage_agent)
+            self.SWA_agent = SWA("SWA_agent", self, self.Res_prei, self.tele_damand, self.tele_supply, self.results_SWA, i)
+            self.agents.append(self.SWA_agent)
+            self.schedule.add(self.SWA_agent)
+            
+            # Schritt-Funktion wird aufgerufen
+            self.step(i)
+        return
+    
+    # Schritt-Funktion für jeden Zeitschritt    
+    def step(self, i):
+        # Ausführen des Zeitschritts
+        self.schedule.step()
+        
+        # Sammeln der Ergebnisse von jedem Agenten
+        self.results_MAN.append(self.MAN_agent.get_opt())
+        self.results_MTA.append(self.MTA_agentget_opt())
+        self.results_UPM.append(self.UPM_agent.get_opt())
+        self.results_SWA.append(self.SWA_agent.get_results())
+        self.results_storage.append(self.storage_agent.get_results())
+        
+        # Berechnung der Produktion und Nachfrage für jedes Unternehmen
+        self.production_MAN = np.array([["wh", self.MAN_agent.get_c_w()[i], self.results_MAN[i][1]], ["electr", self.MAN_agent.get_c_E_el_d()[i]*self.MAN_agent.get_const_E_el_to_h(), self.results_MAN[i][11]], ["gas", self.MAN_agent.get_c_E_g_grid()[i]*self.MAN_agent.get_const_E_g_to_h(), self.results_MAN[12]], ["tele", self.MAN_agent.get_c_t_d()[i], self.results_MAN[i][9]], ["quarter", self.MAN_agent.get_c_q_d()[i], self.results_MAN[i][7]]])
+        self.d_MAN = sum(self.production_MAN[:5])
+        self.q_MAN = self.production_MAN[5]
+        
+        self.production_MTA = np.array([["wh", self.MTA_agent.get_c_w()[i], self.results_MTA[i][1]], ["electr", self.MTA_agent.get_c_E_el_d()[i]*self.MTA_agent.get_const_E_el_to_h(), self.results_MTA[i][11]], ["gas", self.MTA_agent.get_c_E_g_grid()[i]*self.MTA_agent.get_const_E_g_to_h(), self.results_MTA[12]], ["tele", self.MTA_agent.get_c_t_d()[i], self.results_MTA[i][9]], ["quarter", self.MTA_agent.get_c_q_d()[i], self.results_MTA[i][7]]])
+        self.d_MTA = sum(self.production_MTA[:5])
+        self.q_MTA = self.production_MTA[5]
+        
+        self.production_UPM = np.array([["wh", self.UPM_agent.get_c_w()[i], self.results_UPM[i][1]], ["electr", self.UPM_agent.get_c_E_el_d()[i]*self.UPM_agent.get_const_E_el_to_h(), self.results_UPM[i][11]], ["gas", self.UPM_agent.get_c_E_g_grid()[i]*self.UPM_agent.get_const_E_g_to_h(), self.results_UPM[12]], ["tele", self.UPM_agent.get_c_t_d()[i], self.results_UPM[i][9]], ["quarter", self.UPM_agent.get_c_q_d()[i], self.results_UPM[i][7]]])
+        self.d_UPM = sum(self.production_UPM[:5])
+        self.q_UPM = self.production_UPM[5]
+        
+        # Berechnung von Angebot und Nachfrage des Speichers
+        self.demand_storage = self.results_storage[i][0]
+        self.supply_storage = self.results_storage[i][1]
+        
+        if i==0:
+            epsilon_q = self.model.database.optimization_parameter['epsilon_ih']
+            self.epsilon_MAN_t_0 = (self.MAN_agent.calculate_epsilon(epsilon_q, i, self.results_MAN[i][8], self.results_MAN[i][7]))
+            self.epsilon_MTA_t_0 = (self.MTA_agent.calculate_epsilon(epsilon_q, i, self.results_MTA[i][8], self.results_MTA[i][7]))
+            self.epsilon_UPM_t_0 = (self.UPM_agent.calculate_epsilon(epsilon_q, i, self.results_UPM[i][8], self.results_UPM[i][7]))
+            self.epsilon_storage_t_0 = (self.storage_agent.calculate_epsilon(epsilon_q, i, self.results_storage[i][1], self.results_storage[i][0]))
+            self.epsilon_average_t_0 = [self.epsilon_MAN_t_0, self.epsilon_MTA_t_0, self.epsilon_UPM_t_0, self.epsilon_storage_t_0]
+        
+        # Initialisierung und Aufruf der Optimierungsfunktion für den Marktalgorithmus & Speicherung Ergebnis
+        if i==0:
+            self.optimierung_Marketalgorithmus = Optimierung_Marketalgorithmus(self.demand_storage, self.supply_storage, self.production_MAN, self.d_MAN, self.q_MAN, self.production_MTA, self.d_MTA, self.q_MTA, self.production_UPM, self.d_UPM, self.q_UPM, self.epsilon_average_t_0)
+            self.results_P2P_trading.append(self.optimierung_Marketalgorithmus.get_optimales_Ergebnis())    
+        else:    
+            self.optimierung_Marketalgorithmus = Optimierung_Marketalgorithmus(self.demand_storage, self.supply_storage, self.production_MAN, self.d_MAN, self.q_MAN, self.production_MTA, self.d_MTA, self.q_MTA, self.production_UPM, self.d_UPM, self.q_UPM,  self.epsilon_average[i-1])
+            self.results_P2P_trading.append(self.optimierung_Marketalgorithmus.get_optimales_Ergebnis())
+        
+        # Durchführung der Emissionsbilanzierung
+        self.epsilon_MAN.append(self.MAN_agent.calculate_epsilon(self.results_P2P_trading[i][len(self.results_P2P_trading)-1], i, self.results_MAN[i][8], self.results_MAN[i][7]))
+        self.epsilon_MTA.append(self.MTA_agent.calculate_epsilon(self.results_P2P_trading[i][len(self.results_P2P_trading)-1], i, self.results_MTA[i][8], self.results_MTA[i][7]))
+        self.epsilon_UPM.append(self.UPM_agent.calculate_epsilon(self.results_P2P_trading[i][len(self.results_P2P_trading)-1], i, self.results_UPM[i][8], self.results_UPM[i][7]))
+        self.epsilon_storage.append(self.storage_agent.calculate_epsilon(self.results_P2P_trading[i][len(self.results_P2P_trading)-1], i, self.results_storage[i][1], self.results_storage[i][0]))
+        self.epsilon_average.append([self.epsilon_MAN[i], self.epsilon_MTA[i], self.epsilon_UPM[i], self.epsilon_storage[i]])
+        q_t_d = self.results_MAN[i][9] + self.results_MTA[i][9] + self.results_UPM[i][9]
+        q_t_s = self.results_MAN[i][10] + self.results_MTA[i][10] + self.results_UPM[i][10]
+        self.epsilon_SWA.append(self.SWA_agent(self.results_P2P_trading[i][len(self.results_P2P_trading)-1], i, q_t_s, q_t_d))
+        return
+    
+marktalgorithmus = Marktalgorithmus(simulation_data, level_t_0) # Methodenaufruf aus der main-Datei mit simulation_data, level_t_0
--- SWA.py
+++ SWA.py
@@ -1,70 +1,77 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 20 13:08:06 2023
-
-@author: juliu
-"""
-
+# Importieren der benötigten Bibliotheken
 import mesa
 import numpy as np
 import gurobipy as gp
 from gurobipy import GRB
 
-class Storage(mesa.Agent):
-        def __init__(self, unique_id, model):   
+# Klasse für den SWA-Agenten, die von mesa.Agent erbt
+class SWA(mesa.Agent):
+        def __init__(self, unique_id, model, Res_pre, tele_demand, tele_supply, results_SWA, t):   
             super().__init__()
             
-            # set agent parameter
+            # Anlegen der Agentenparameter
             self.model=model
-
-            # time index
-            ti = self.model.time_index
-            rti = list(range(len(ti)))
-            
-            # non-decision variables, used for tracking results
+            self.t=t
+            self.Res_pre = Res_pre
+            self.tele_demand = tele_demand
+            self.tele_supply = tele_supply
+            self.results_SWA = results_SWA
             self.cap_max_ih = self.model.database.optimization_parameter['cap_max_ih']
-            self.Res_pre = self.model.database.optimization_parameter['Res_pre']
             self.e_q_s = self.model.database.optimization_parameter['e_q_s']
             self.e_t_s = self.model.database.optimization_parameter['e_t_s']
             self.c_ih_d = self.model.database.optimization_parameter['c_ih_d']
             self.c_q_d = self.model.database.optimization_parameter['c_q_d']
-            self.e_q = self.model.database.optimization_parameter['e_q']
-            self.e_ih = self.model.database.optimization_parameter['e_ih']
+            self.epsilon_q = self.model.database.optimization_parameter['epsilon_q']
+            self.epsilon_ih = self.model.database.optimization_parameter['epsilon_ih']
             self.epsilon_max_kWh = self.model.database.optimization_parameter['epsilon_max_kWh']
-            
-            # update data and set up model
-            self.update_data()
-            self.setup_model()
-            
-        def update_data(self):
-            #code
-            return
-            
-        def setup_model(self):
-            # time index
+
+            # Anlegen Zeitindex
             ti = self.model.time_index
             rti = list(range(len(ti)))
             
-            # storage model
-            self.m = gp.Model(f'storage_{self.unique_id}')
-            self.m.Params.OutputFlag = 0
+            # Berechunung der Wärme-Residualmengen des Quartiers aus der Vorperiode
+            for i in rti:
+                if i<=len(self.Res_pre):
+                    continue #Falls für Periode i ein Wert für Res_pre existiert -> keine Veränderung
+                else:
+                    self.Res_pre.append(self.Res_pre[self.Res_pre-1]) #Falls für Periode i kein Wert für Res_pre existiert -> Annahmen: Res_pre für Periode i = Res_pre der Vorperiode
+            return
+        
+        # Methode zur Initialisierung des Optimierungsmodells
+        def setup_model(self):
+            # Anlegen Zeitindex
+            ti = self.model.timepsilon_index
+            rti = list(range(len(ti)))
             
-            # decision variables, used in optimization
+            # Initialisierung des Modells
+            self.m = gp.Model(f'storage_{self.unique_id}')
+            self.m.Params.OutputFlag = 0 #Output-Parameter werden nicht ausgegeben, wenn 0
+            
+            # Anlegen der Entscheidungsvariablen
+            # m.addVars mit Zeitindex rti -> Die EV wird als Array mit Eintrag für jede Periode angelegt
+            # m.addVar -> eine einzelne EV für der Gesamtzeitraum
             self.q_ih_d = self.m.addVars(
-                rti, vtype=GRB.CONTINUOUS, name='q_ih', lb=0, ub=self.cap_max_ih)
+                rti, vtype=GRB.CONTINUOUS, name='q_ih_d', lb=0, ub=self.cap_max_ih)
             self.q_q_s = self.m.addVars(
                 rti, vtype=GRB.CONTINUOUS, name='q_q_s', lb=0, ub=GRB.INFINITY)
             self.q_q_d = self.m.addVars(
                 rti, vtype=GRB.CONTINUOUS, name='q_q_d', lb=0, ub=GRB.INFINITY)
             self.q_t_s = self.m.addVars(
                 rti, vtype=GRB.CONTINUOUS, name='q_t_s', lb=0, ub=GRB.INFINITY)
-            self.Epsilon_max = self.m.addVar(
-                vtype=GRB.CONTINUOUS, name='Epsilon_max', lb=0, ub=GRB.INFINITY)
+            self.epsilon_max = self.m.addVar(
+                vtype=GRB.CONTINUOUS, name='epsilon_max', lb=0, ub=GRB.INFINITY)
+            self.epsilon_total = self.m.addVar(
+                vtype=GRB.CONTINUOUS, name='epsilon_total', lb=0, ub=GRB.INFINITY)
             
-            # target funktion
+            # Anlegen der Zielfunktion
             self.m.setObjective(gp.quicksum(self.q_q_s[i]*self.e_q_s[i]- self.q_t_s[i]*self.e_t_s[i]- self.q_ih_d[i]*self.c_ih_d[i] -self.q_q_d[i]*self.c_q_d[i] for i in rti), GRB.MAXIMIZE)
+            
+            # Anlegen der Variablen für die Emissionsbilanzierung
+            self.epsilon_average_t = [0]*rti
+            self.epsilon_total_t = [0]*rti
 
-            # constraints
+            # Anlegen der Nebenbedingungen              
             for j in rti:
                 if j==0:
                     self.m.addConstr(self.q_q_d[j]>=0, name=f'q_q_d_min_1{j}')
@@ -73,24 +80,63 @@ class Storage(mesa.Agent):
                     self.m.addConstr(self.q_q_d[j] >= self.Res_pre[j], name=f'q_q_d_min_Res>=0{j}')
                 elif self.Res_pre <0:
                     self.m.addConstr(self.q_q_s[j] >= self.Res_pre[j], name=f'q_q_s_min_Res<=0{j}')
-            self.m.addConstr(self.Epsilon_max==self.epsilon_max_kWh_max*gp.quicksum(self.q_q_d[j]+self.q_ih_d[j] for j in rti), name='calculation_Epsilon_max')
+            self.m.addConstr(self.epsilon_max==self.epsilon_max_kWh*gp.quicksum(self.q_q_d[j]+self.q_ih_d[j] for j in rti), name='calculation_epsilon_max')
             self.m.addConstr(0<=gp.quicksum(self.q_q_d[j]+self.q_ih_d[j]-self.q_t_s-self.q_q_s for j in rti), name='demand>=supply')
-            self.m.addConstr(self.Epsilon_max <= gp.quicksum(self.q_q_d[j]*self.e_q+self.q_ih_d[j]*self.e_ih for j in rti), name='Emissions')
+            self.m.addConstr(self.epsilon_total == gp.quicksum(self.q_q_d[j]*self.epsilon_q+self.q_ih_d[j]*self.epsilon_ih[j] for j in rti), name='Emissions')
+            self.m.addConstr(self.epsilon_max >= self.epsilon_total, name='check_Emissions')
+            
+            # Fixierung der Werte der EVs aller Vorperioden
+            if self.t ==1:
+                # Falls zweite Periode (Index = 1): Fixierung der Handelsmengen für Periode 1 (Index = 0)
+                self.m.addConstr(self.q_q_d[0]==self.tele_demand[0])
+                self.m.addConstr(self.q_q_s[0]==self.tele_supply[0])
+            elif self.t>1:
+                for i in range(0, self.t-1):
+                    # Alle Entscheidungsvariablen der indirekten Vorperioden fixieren
+                    self.m.addConstr(self.q_ih_d[i]==self.results_SWA[i][0])
+                    self.m.addConstr(self.q_q_d[i]==self.results_SWA[i][1])
+                    self.m.addConstr(self.q_q_s[i]==self.results_SWA[i][2])
+                    self.m.addConstr(self.q_t_s[i]==self.results_SWA[i][3])
+                    self.m.addConstr(self.epsilon_max[i]==self.results_SWA[i][4])
+                    self.m.addConstr(self.epsilon_total[i]==self.results_SWA[i][5])
+                # Für direkte Vorperiode: Fixierung der Handelsmengen für Vorperiode    
+                self.m.addConstr(self.q_q_d[0]==self.tele_demand[self.t-1])
+                self.m.addConstr(self.q_q_s[0]==self.tele_supply[self.t-1])
+            return
+        
+        # Berechnung der Emissionen
+        def calculate_epsilon(self, epsilon_average_q, i, q_s, q_d):
+            # Emissionen = Emissionen Eigenproduktion + CO2-Äquivalent der gekauften Wärmemenge aus dem Quartiershandel - CO2-Äquivalent der verkauften Wärmemenge aus dem Quartiershandel
+            self.epsilon_total_t[i] = self.epsilon_ih[i] * self.q_ih_d[i] + epsilon_average_q * q_d - epsilon_average_q *q_s
+            self.epsilon_average_t[i] = self.epsilon_total_t[i]/(self.q_ih_d[i]+q_d-q_s)
+            return self.epsilon_average_t[i]
+        
+        # Schritt-Funktion zur Durchführung eines Zeitschritts
+        def step(self):
+            self.setup_model()
             self.results = self.run_model()
             return
         
+        # Methode zur Rückgabe der Ergebnisse
+        def get_results(self):
+            return self.results
+        
+        # Optimierung zum Lösen des Modells
         def run_model(self):
-            rti = list(range(len(self.model.time_index)))
+            # Optimierung des Modells ausführen
             self.m.optimize()
-
+            
+            # Rückgabe der Ergebnisse, falls Optimium gefunden
             if self.m.status == GRB.OPTIMAL:
                 print("Optimal solution found.")
                 # Get values of decision variables
-                results = np.array([var.X for var in self.m.getVars()])
-                value_target = self.m.ObjVal
-                return results, value_target
+                #results = np.array([var.X for var in self.m.getVars()])
+                results = self.q_ih_d.x[self.t], self.q_q_d.x[self.t], self.q_q_s.x[self.t], self.q_t_s.x[self.t], self.epsilon_max.x[self.t], self.epsilon_total.x[self.t]
+                #value_target = self.m.ObjVal
+                return results
+            # Rückgabe von None-Wert, falls kein Optimium gefunden
             else:
                 print("Optimization did not converge to an optimal solution.")
                 self.m.computeIIS()
                 self.m.write("model.ilp")
-                return None, 0
+                return None
